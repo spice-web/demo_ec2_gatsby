1db714d0e09a828c77a8413ca9792f0d7b7ccf7b
spice-web <admin@spice-web.jp>
2022-10-07 17:21:12 +0900

    add data-folder /page/project.js

--- src/pages/voice.js
+++ src/pages/voice.js
@@ -7,15 +7,23 @@ import Seo from "../components/seo";
 const VoicePage = ({ data }) => (
   <Layout>
 
-    {/* カードブロック */}
-    {data.allMicrocmsVoice.edges.map(({ node }) => (
-      <div className="">
-        <h3>お客様の声#{node.number}</h3>
-          <p>{node.user_voice}</p>
-        <h3>サンパーキングより</h3>
-          <p>{node.reply}</p>
+    <div className="content__wrap">
+
+      <div className="content__inner">
+        {/* カードブロック */}
+        {data.allMicrocmsVoice.edges.map(({ node }) => (
+          <div className="voice__wrapper">
+            <h3>お客様の声#{node.number}</h3>
+            <p>{node.user_voice}</p>
+            <h3>サンパーキングより</h3>
+            <p>{node.reply}</p>
+          </div>
+        ))}
       </div>
-    ))}
+
+    </div>
+
+
   </Layout>
 )
 
