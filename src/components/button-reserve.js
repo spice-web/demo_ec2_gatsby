c51e7f283ae801f33d2e212c98cbbbae86d6e0d6
spice-web <admin@spice-web.jp>
2022-10-17 17:37:16 +0900

    index css

--- src/components/button-reserve.js
+++ src/components/button-reserve.js
@@ -8,11 +8,11 @@ const ButtonReserve = ({ bg }) => (
   <div className={styles.button__reserve__wrap}  data-bg={bg}>
     <div className={styles.button__reserve} data-type="web" >
       <p className={styles.button__reserve__title}>▼ 通常料金から38%OFF! ▼</p>
-      <Link to="/" className={styles.button__reserve__link}>今すぐ予約</Link>
+      <Link to="https://twitter.com/sunparking" className={styles.button__reserve__link} target="_blank" rel="noreferrer noopener">今すぐ予約</Link>
     </div>
     <div className={styles.button__reserve} data-type="line" >
       <p className={styles.button__reserve__title}>▼ 洗⾞無料クーポン配布中 ▼</p>
-      <Link to="/" className={styles.button__reserve__link}>LINE予約</Link>
+      <Link to="https://page.line.me/165idnoa" className={styles.button__reserve__link} target="_blank" rel="noreferrer noopener">LINE予約</Link>
     </div>
   </div>
 )
