beea83e7424470bf812e2399742c2f69a1960dee
spice-web <admin@spice-web.jp>
2022-11-24 17:04:58 +0900

    update flex-form

--- src/pages/form_flex.js
+++ src/pages/form_flex.js
@@ -4,17 +4,25 @@ import Seo from "../components/seo";
 
 const FormFlex = () => (
   <Layout>
-    <Seo title="お問い合わせありがとうございます" />
+    <Seo title="お問い合わせ" />
     <>
     <form method="post"
       action="https://www.flexyform.com/f/1182875604167c7dcf13359e5c66df8294902e4c">
         <p>
-            <label>Fullname</label>
+            <label>お名前※必須</label>
             <input type="text" name="fullname" />
         </p>
         <p>
-            <label>Email</label>
+            <label>フリガナ※必須</label>
+            <input type="text" name="kana" />
+        </p>
+        <p>
+            <label>メールアドレス※必須</label>
             <input type="email" name="_reply_to" />
+        </p>
+        <p>
+        <label>お問い合わせ内容※必須</label>
+            <textarea name="message"></textarea>
         </p>
         <button type="submit">Send</button>
     </form>
