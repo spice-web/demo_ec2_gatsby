1bc950b6898dfa7d09d1199e244866f43a434130
spice-web <admin@spice-web.jp>
2022-12-09 14:13:43 +0900

    noindex

--- src/components/seo.js
+++ src/components/seo.js
@@ -38,6 +38,7 @@ function Seo({ description, title, children }) {
       <meta name="twitter:creator" content={site.siteMetadata?.author || ``} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={metaDescription} />
+      <meta name="robots" content="noindex" />
       {children}
     </>
   )
