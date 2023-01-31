2a2547d8c6735a0298f1b0adbf0b392431ce13d5
spice-web <admin@spice-web.jp>
2023-01-31 17:48:51 +0900

    update previewPage.js bodyない書き換え

--- src/pages/previewPage.jsx
+++ src/pages/previewPage.jsx
@@ -2,10 +2,27 @@ import * as React from "react"
 import { useState, useEffect } from "react"
 import { getSearchParams } from "gatsby-query-params"
 import queryString from "query-string"
-//...その他コンポーネントなど
 
+//...その他コンポーネントなど
 import Seo from "../components/seo"
 import Layout from "../components/layout"
+import ShareButtonList from "../components/ShareButtonList"
+
+// サイドバー
+import Sidebar from "../components/Sidebar"
+//  CSS
+import * as infoStyles from "../styles/_information.module.scss"
+//　microCMS用画像CSS
+import * as Styles from "../styles/_info_post.module.scss"
+
+// 日付取得
+import dayjs from "dayjs"
+import utc from "dayjs/plugin/utc"
+import timezone from "dayjs/plugin/timezone"
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
+// dayjs.utc(publishedAt).tz("Asia/Tokyo").format("YYYY-MM-DD")
 
 function PreviewPage() {
   //microCMS側に設定するurlはhttps://xxxxxx.com/previewPage/?contentId={CONTENT_ID}&draftKey={DRAFT_KEY}みたいな感じ
@@ -58,11 +75,40 @@ function PreviewPage() {
       />
       {/* コンテナ */}
       <div className="content__wrap content__pd">
-        <div
-          dangerouslySetInnerHTML={{
-            __html: postData ? postData.title : "見つかりませんでした",
-          }}
-        ></div>
+        <div className={infoStyles.inner}>
+          <div className={infoStyles.mainInner}>
+            {/* 表題ブロック */}
+            <div
+              className={infoStyles.title}
+              dangerouslySetInnerHTML={{
+                __html: postData ? postData.title : "タイトルなし",
+              }}
+            ></div>
+
+            {/* 記事詳細ブロック */}
+            <div className={Styles.post}>
+              <div
+                className=""
+                dangerouslySetInnerHTML={{
+                  __html: postData ? postData.date : "日付なし",
+                }}
+              ></div>
+              <div
+                className=""
+                dangerouslySetInnerHTML={{
+                  __html: postData ? postData.body : "テキスト無し",
+                }}
+              />
+            </div>
+
+            {/* シェアボタン */}
+            <ShareButtonList
+              url={`http://ec2-54-238-164-111.ap-northeast-1.compute.amazonaws.com/information/${data.microcmsInformation.informationId}`}
+            />
+          </div>
+
+          <Sidebar />
+        </div>
       </div>
     </Layout>
   )
